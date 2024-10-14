96b27e7754b6f0d6f820aedf815d5249629ee3f2
przemyslawgesieniecasit <przemyslaw.gesieniec@gmail.com>
2024-10-14 17:53:35 +0200

    jdbc session implemeted

--- src/main/java/pl/akademiaspecjalistowit/podstawyspringsecurity/configuration/SecurityConfig.java
+++ src/main/java/pl/akademiaspecjalistowit/podstawyspringsecurity/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.akademiaspecjalistowit.podstawyspringsecurity.configuration;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,27 +21,20 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(authz -> authz
                 .requestMatchers("/books").permitAll()
-                .requestMatchers("/students").hasRole("ADMIN")
+                .requestMatchers("/students/addButton").hasRole("ADMIN")
+                .requestMatchers("/h2-console/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .formLogin(withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("pwd")
-            .roles("USER")
-            .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("pwd")
-            .roles("ADMIN")
-            .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 }
